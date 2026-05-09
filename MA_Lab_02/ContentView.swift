e87b195f06fc256b3c9b4c5b464e3e2fe7417931
Bojan Stefanovski <bojanpece0@gmail.com>
2026-05-09 15:42:49 +0200

    Initial Commit

---
+++ MA_Lab_02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MA_Lab_02
+//
+//  Created by Bojan Stefanovski on 09/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MA_Lab_02/MA_Lab_02App.swift
@@ -0,0 +1,17 @@
+//
+//  MA_Lab_02App.swift
+//  MA_Lab_02
+//
+//  Created by Bojan Stefanovski on 09/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MA_Lab_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
